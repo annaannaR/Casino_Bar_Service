c1ec97a8d5220085a253c5fe54a7e37d0a2f68da
annaannaR <anna.rogiewicz@gmail.com>
2022-01-26 19:36:33 +0100

    Start of aplication

---
+++ src/main/java/pl/casinoBarService/CasinoBarServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.casinoBarService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasinoBarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasinoBarServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/casinoBarService/CasinoBarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.casinoBarService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasinoBarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
